0542d653917b3d9d20b697059cf7dc262744c7bb
Kurt Rose <kurose@paypal.com>
2012-05-23 12:20:09 -0700

    gevent multiple worker processes hello world running

--- gunicorn_wrangler.py
+++ gunicorn_wrangler.py
@@ -9,9 +9,10 @@ def run_app(app, address="127.0.0.1:8000"):
     '''
     class App(Application):
         def init(self, parser, opt, args):
-            #self.cfg.set('worker_class', 'ggevent')
+            self.cfg.set('worker_class', 'gevent')
+	    self.cfg.set('workers', 6)
             self.cfg.set('bind', address)
-        
+
         def load(self):
             return app
     
@@ -19,9 +20,10 @@ def run_app(app, address="127.0.0.1:8000"):
 
 
 if __name__ == '__main__':
+    import os
 
     def hello_wsgi(environ, start_response):
         start_response('200 OK', [('Content-Type', 'text/plain')])
-        yield 'Hello World\n'
+        yield 'Hello World gevent world! \n' + str(os.getpid())
     
     run_app(hello_wsgi, "0.0.0.0:8888")
